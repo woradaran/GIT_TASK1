9798b2134dd190ffbeb66c463b2757cc7cfef1a5
kr-pound <79203105+kr-pound@users.noreply.github.com>
2021-02-18 18:06:10 +0700

    Update Transportation.py

--- Transportation.py
+++ Transportation.py
@@ -1,7 +1,7 @@
 class Transportation(object):
    """Abstract base class"""
 
-   def __init__( self, start, end, distance ):
+   def __init__( self, start, end, distance, station = 0 ):
       if self.__class__ == Transportation:
          raise NotImplementedError
       self.start = start
@@ -14,9 +14,9 @@ class Transportation(object):
 
 
 class Walk( Transportation ):
-
-   def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
+   
+   def __init__( self, start, end, distance, station = 0 ):
+      super.__init__( self, start, end, distance, station = 0)
 
    def find_cost( self ):
       return 0
@@ -26,26 +26,31 @@ class Taxi(Transportation):
     def __init__(self, start, end, rate, distance):
         super().__init__(start, end, distance)
         self._rate = rate
+        self._distance = distance
         
     def find_cost(self):
-        self._cost = super().getD() * self._rate
+        self._cost = self._distance * self._rate
         return self._cost
    
    
 class Train(Transportation):
-   def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
+   def __init__( self, start, end, distance, station ):
+      Transportation.__init__( start, end, distance)
+      self._station = station * 5
 
+   def find_cost(self):
+        self._cost = self._station
+        return self._cost
 
    
 # main program
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
